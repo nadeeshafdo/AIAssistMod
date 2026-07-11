239cb50ddb59586b28b0ed854ed9f26e5884506a
Nadeesha Fernando <itsme@nadeeshafdo.dev>
2026-07-11 11:54:36 +0530

    chore: remove test file

--- TestPower.java
+++
@@ -1,6 +0,0 @@
-import mindustry.world.blocks.power.PowerGenerator;
-public class TestPower {
-    public void test(PowerGenerator.GeneratorBuild b) {
-        float p = b.getPowerProduction();
-    }
-}
